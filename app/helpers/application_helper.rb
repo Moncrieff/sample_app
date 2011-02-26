1e74c8014dd29954f3f5b965519253069b6c9fa7
Dmitry Veremchuk <dmitry.veremchuk@me.com>
2011-02-26 16:44:27 +0300

    Links on the layout test added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+#Keeps info on site logo
+def logo
+	logo = image_tag("logo.png", :alt => "Sample App", :class => "round")	
+end 
+
 # Returns a title on per-page basis
 def title
 	base_title = "Ruby on Rails Tutorial Sample App"
